e17da10f0bf416a5956dc029d4bb15a877dbe512
Prakash Adhikari <pa7846665@gmail.com>
2023-10-27 20:36:09 +0530

    maxsumsub

--- daily dsa/maximum_sum_subarray.cpp
+++ daily dsa/maximum_sum_subarray.cpp
@@ -1,23 +1,54 @@
+
+// all possible subarrays of array
+
+// #include <bits\stdc++.h>
+
+// using namespace std;
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3};
+
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     for (int size = 1; size <= n; ++size) // size=1 ; 1 <=3
+//     {
+//         for (int i = 0; i <= n - size; ++i) // i=0 ; 0<=2
+//         {
+//             for (int j = i; j < i + size; ++j) // j=0 ; j<0+1
+//             {
+//                 cout << arr[j] << " ";
+//             }
+
+//             cout << endl;
+//         }
+//     }
+//     return 0;
+// }
+
 #include <bits\stdc++.h>
 using namespace std;
 
-int main()
+int Maxsumsubarray(int arr[], int n)
 {
-    int arr[] = {1, 2, 3};
-
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    for (int size = 1; size <= n; ++size) // size=1 ; 1 <=3
+    int maxi = INT16_MIN;
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i <= n - size; ++i) // i=0 ; 0<=2
+        int prefix = 0;
+        for (int j = i; j < n; j++)
         {
-            for (int j = i; j < i + size; ++j) // j=0 ; j<0+1
-            {
-                cout << arr[j] << " ";
-            }
-
-            cout << endl;
+            prefix += arr[j];
+            maxi = max(maxi, prefix);
         }
     }
-    return 0;
+    return maxi;
+}
+
+int main()
+{
+
+    int arr[] = {10, -4, 5, -10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << Maxsumsubarray(arr, n);
 }
